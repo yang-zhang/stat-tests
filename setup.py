cd288531ce406ec26c0dd5cf591510908c5925b4
yang-zhang <zhangyangcolostate@gmail.com>
2017-09-27 23:01:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0',
+    version='0.0.0post1',
 
     description='Easy-to-use statistical tests in Python',
     # long_description=long_description,
@@ -48,7 +48,7 @@ setup(
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Statistics :: Statistical Testing',
+        'Topic :: Scientific/Engineering :: Mathematics',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
