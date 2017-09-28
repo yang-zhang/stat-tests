6893c57a1c25be55c07ecada5d4071ffd7b79ddd
yang-zhang <zhangyangcolostate@gmail.com>
2017-09-27 21:56:49 -0400

    Update

--- stat_tests.py
+++ stat_tests.py
@@ -5,7 +5,20 @@ import scipy.stats
 import statsmodels.stats.proportion
 
 
-def find_confidence_interval(x, n, confidence_interval_size):
+def single_proportion_confidence_interval(x, n, confidence_interval_size=0.95):
+    '''Single proportion confidence interval.
+
+    For example, if x out of n customers clicked the link , what is the \
+    confidence interval of click through rate.
+
+    Parameters:
+    x: number of samples of interest
+    n: number of total samples
+    confidence_interval_size: 0 to 1
+
+    Returns:
+    confidence_interval
+    '''
     p_hat = x / n
     standard_error = np.sqrt(p_hat * (1 - p_hat) / n)
     alpha = 1 - confidence_interval_size
@@ -15,15 +28,28 @@ def find_confidence_interval(x, n, confidence_interval_size):
     return confidence_interval
 
 
-def single_proportion_ztest(x, n, p_hypo=0.5, prop_var=None, one_side=False):
+def single_proportion_test(x, n, p_hypo=0.5, one_side=False):
+    '''Single proportion ztest.
+
+    For example, if x out of n customers are men, and the hypothesis is that\
+    50% of the customers are men, does data support this hypothesis?
+
+    Parameters:
+    x: number of samples of interest
+    n: number of total samples
+    p_hypo: hypothetical proportion
+    one_side: if True, do one side test
+
+    Returns:
+    z: z score
+    p: p_value
+    '''
     p_hat = x / n
-    if not prop_var:
-        prop_var = p_hat
-    standard_error = np.sqrt(prop_var * (1 - prop_var) / n)
+    standard_error = np.sqrt(p_hat * (1 - p_hat) / n)
     z = (p_hat - p_hypo) / standard_error
     p_value = 1 - scipy.stats.norm.cdf(abs(z))
     if not one_side:
         p_value *= 2
     return z, p_value
 
-def
+
