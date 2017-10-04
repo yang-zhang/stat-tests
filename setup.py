f845a3fb76dddd441947eacc0395e2d1f5219dd3
Yang Zhang <yang.zhang.ds@gmail.com>
2017-10-04 08:06:41 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.0post1',
+    version='0.0.0post2',
 
     description='Easy-to-use statistical tests in Python',
     # long_description=long_description,
